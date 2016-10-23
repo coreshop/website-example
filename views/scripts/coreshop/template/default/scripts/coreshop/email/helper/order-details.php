603e92724bd665bb22a1790655dbfb64da2fc3ef
dpfaffenbauer <dominik@lineofcode.at>
2016-10-23 13:39:30 +0200

    case coreshop\coreshop#100 implement guest order tracking

--- views/scripts/coreshop/template/default/scripts/coreshop/email/helper/order-details.php
+++ views/scripts/coreshop/template/default/scripts/coreshop/email/helper/order-details.php
@@ -1,1 +1,11 @@
 <?=$this->template("user/helper/order-items.php");?>
+
+<?php
+    if($this->order->getCustomer() instanceof CoreShop\Model\User) {
+        if($this->order->getCustomer()->getIsGuest()) {
+            ?>
+            <a href="<?=\CoreShop::getTools()->url(array("act" => "guest-order-tracking", "lang" => $this->language), "coreshop_user")?>"><?=$this->translate("Track your order")?></a>
+            <?php
+        }
+    }
+?>
