9ada4923cff6952419d5b64a6838f444de7de5bf
dpfaffenbauer <dominik@lineofcode.at>
2016-10-27 08:46:14 +0200

    add stepCount to range filter

--- views/scripts/coreshop/template/default/scripts/coreshop/product/filter/range.php
+++ views/scripts/coreshop/template/default/scripts/coreshop/product/filter/range.php
@@ -18,7 +18,7 @@
                 </div>
 
                 <div class="col-xs-6">
-                    <input title="<?=$this->translate($this->label)?>" type="text" name="<?=$this->fieldname?>" class="range-slider span2" value="" data-slider-min="<?=$minValue?>" data-slider-max="<?=$maxValue?>" data-slider-step="5" data-slider-value="[<?=$this->currentValueMin?>,<?=$this->currentValueMax?>]"/>
+                    <input title="<?=$this->translate($this->label)?>" type="text" name="<?=$this->fieldname?>" class="range-slider span2" value="" data-slider-min="<?=$minValue?>" data-slider-max="<?=$maxValue?>" data-slider-step="<?=$this->stepCount?>" data-slider-value="[<?=$this->currentValueMin?>,<?=$this->currentValueMax?>]"/>
                 </div>
 
                 <div class="col-xs-3">
